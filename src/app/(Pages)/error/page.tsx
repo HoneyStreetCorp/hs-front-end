fe7561af060a6d69a75a95c8ec83c201668bbbe1
DESKTOP-1AOFNQ8\a <guswhddlf11@gmail.com>
2023-10-18 18:19:14 +0900

    Feat: 에러페이지 추가

---
+++ src/app/(Pages)/error/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function ErrorPage() {
+    return (
+        <div className="flex h-full flex-col items-center justify-center bg-gradient-to-t pt-4">
+            <img src="/images/error_img.png" alt="pp" className="mx-auto" />
+        </div>
+    );
+}
