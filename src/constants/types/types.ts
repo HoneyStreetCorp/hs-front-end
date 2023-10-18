0ed258b46c8d92ce72253f065b531d3477c3e5ac
DESKTOP-1AOFNQ8\a <guswhddlf11@gmail.com>
2023-10-18 18:18:16 +0900

    Chore: recoil type 선언 및 progressbar 색상 수정

--- src/constants/types/types.ts
+++ src/constants/types/types.ts
@@ -4,3 +4,16 @@ export type Question = {
     body: string | null;
     choices: string[];
 };
+
+export type confirmModalStateProps = {
+    isOpen: boolean;
+    title: string;
+    content: string;
+    toggleClickConfirmBtn: Function;
+    toggleClickCancelBtn: Function;
+};
+export type questionStateProps = {
+    questions: Question[];
+    maxPage: number;
+    currentPage: number;
+};
